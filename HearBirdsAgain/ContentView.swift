7ba455c20ca3f802704745d7b3842fc13e39c082
Harold Mills <harold.mills@gmail.com>
2022-03-25 10:39:07 -0400

    Initial Commit

---
+++ HearBirdsAgain/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HearBirdsAgain
+//
+//  Created by Harold Mills on 3/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HearBirdsAgain/HearBirdsAgainApp.swift
@@ -0,0 +1,17 @@
+//
+//  HearBirdsAgainApp.swift
+//  HearBirdsAgain
+//
+//  Created by Harold Mills on 3/25/22.
+//
+
+import SwiftUI
+
+@main
+struct HearBirdsAgainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
