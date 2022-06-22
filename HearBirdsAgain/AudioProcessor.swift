c70c5c5cde2d5c237779fadda2126a14aa58bc73
Harold Mills <harold.mills@gmail.com>
2022-06-22 15:23:51 -0400

    Make audio processor the source of truth for the SongFinder state.

--- HearBirdsAgain/AudioProcessor.swift
+++ HearBirdsAgain/AudioProcessor.swift
@@ -36,31 +36,38 @@ class AudioProcessor: ObservableObject {
     @Published var cutoff = 0 {
         didSet {
             setAudioUnitParam(key: "cutoff", value: AUValue(cutoff))
+            restartIfRunning()
         }
     }
     
     @Published var pitchShift = 2 {
         didSet {
             setAudioUnitParam(key: "pitchShift", value: AUValue(pitchShift))
+            restartIfRunning()
         }
     }
     
     @Published var windowType = WindowType.Hann {
         didSet {
             setAudioUnitParam(key: "windowType", value: AUValue(windowType.rawValue))
+            restartIfRunning()
         }
     }
     
     @Published var windowSize = 20 {
         didSet {
             setAudioUnitParam(key: "windowSize", value: AUValue(windowSize))
+            restartIfRunning()
         }
     }
     
     @Published var gain: AUValue = 0 {
         didSet {
-            getAudioUnitParam(key: "gain").value = gain;
-            // setAudioUnitParam(key: "gain", value: AUValue(gain))
+            // Note that unlike for all of the other SongFinder parameters
+            // we do not need to restart here here since the SongFinder
+            // audio unit can respond to changes in the value of this
+            // parameter while running.
+            setAudioUnitParam(key: "gain", value: AUValue(gain))
         }
     }
     
@@ -71,8 +78,8 @@ class AudioProcessor: ObservableObject {
     
     init() {
         configureAudioEngine()
-        initializeState()
-        showInputSampleRate()
+        setSongFinderState()
+        // showInputSampleRate()
     }
 
     
@@ -90,46 +97,35 @@ class AudioProcessor: ObservableObject {
     }
     
     
-    private func initializeState() {
-        
-        self.cutoff = Int(getAudioUnitParam(key: "cutoff").value)
-        
-        self.pitchShift = Int(getAudioUnitParam(key: "pitchShift").value)
-        
-        let windowTypeParam = getAudioUnitParam(key: "windowType")
-        self.windowType = WindowType(rawValue: windowTypeParam.value) ?? WindowType.Hann
-        
-        self.windowSize = Int(getAudioUnitParam(key: "windowSize").value)
-        
-        self.gain = getAudioUnitParam(key: "gain").value
-        
+    private func setSongFinderState() {
+        setAudioUnitParam(key: "cutoff", value: AUValue(cutoff))
+        setAudioUnitParam(key: "pitchShift", value: AUValue(pitchShift))
+        setAudioUnitParam(key: "windowType", value: AUValue(windowType.rawValue))
+        setAudioUnitParam(key: "windowSize", value: AUValue(windowSize))
+        setAudioUnitParam(key: "gain", value: AUValue(gain))
+        restartIfRunning()
     }
     
     
-    private func getAudioUnitParam(key: String) -> AUParameter {
+    private func setAudioUnitParam(key: String, value: AUValue) {
         
         // We force unwrap the parameter tree and the parameter in this
         // method since we know by design that a SongFinder audio unit
         // has a parameter tree, and we assume that the parameter tree
         // includes a parameter for the specified key.
-        
         let parameterTree = songFinder.auAudioUnit.parameterTree!
-        return parameterTree.value(forKey: key) as! AUParameter
+        let parameter = parameterTree.value(forKey: key) as! AUParameter
+        
+        parameter.value = value
         
     }
     
     
-    private func setAudioUnitParam(key: String, value: AUValue) {
-        getAudioUnitParam(key: key).value = value
-        restartIfRunning()
-    }
-    
-    
-    private func showInputSampleRate() {
-        let input = engine.inputNode
-        let format = input.inputFormat(forBus: 0)
-        logger.log()
-        logger.log("AudioProcessor.showInputSampleRate \(format.sampleRate)")
+    func restartIfRunning() {
+        if (running) {
+            stop()
+            start()
+        }
     }
     
     
@@ -157,11 +153,11 @@ class AudioProcessor: ObservableObject {
     }
     
     
-    func restartIfRunning() {
-        if (running) {
-            stop()
-            start()
-        }
+    private func showInputSampleRate() {
+        let input = engine.inputNode
+        let format = input.inputFormat(forBus: 0)
+        logger.log()
+        logger.log("AudioProcessor.showInputSampleRate \(format.sampleRate)")
     }
     
     
