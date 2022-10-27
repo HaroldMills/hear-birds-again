afd82726cfe8ca5ee0b95e191683eb1513af43f8
Harold Mills <harold.mills@gmail.com>
2022-10-27 13:33:48 -0400

    Edit support info page.

--- HearBirdsAgain/SupportInfoPage.swift
+++ HearBirdsAgain/SupportInfoPage.swift
@@ -18,7 +18,7 @@ struct SupportInfoPage: View {
                 InfoPageTitle("Support")
                 
                 Text("""
-If you experience technical problems with ***Hear Birds Again*** or have questions about the functioning of the app, please feel free to email us at support@hearbirdsagain.org.
+If you experience technical problems with ***Hear Birds Again*** or have questions about how to use it, please feel free to email us at support@hearbirdsagain.org.
 
 There is also a ***Hear Birds Again*** [Google Groups email list](https://groups.google.com/g/hear-birds-again) that you can [join](https://hearbirdsagain.org/users-forum/) to communicate with other users and the development team. We hope to see you there!
 """)
