52d055ec81c56b9f17acea72811d024e0d705a14
Harold Mills <harold.mills@gmail.com>
2022-11-03 10:03:51 -0400

    Add happy user picture to welcome page.

--- HearBirdsAgain/WelcomeInfoPage.swift
+++ HearBirdsAgain/WelcomeInfoPage.swift
@@ -19,8 +19,26 @@ struct WelcomeInfoPage: View {
                 
                 Text("""
 ***Hear Birds Again*** is an iOS app for bird enthusiasts who can no longer hear high-pitched bird sounds, but who can still hear lower-pitched sounds reasonably well. In brief, it works by shifting high-pitched sounds into a lower range where you can hear them.
+""")
+                .padding()
 
-Both the app and its website [hearbirdsagain.org](https://hearbirdsagain.org) include documentation pertaining to the app. *If you want to make effective use of the app, please take the time to read all of the documentation.* The in-app documentation includes detailed descriptions of the functions of app controls, but defers to the website for documentation about many other topics, such as [the problem the app solves](https://hearbirdsagain.org/problem-and-solutions/) and the [binaural headset](https://hearbirdsagain.org/binaural-headset/) that we strongly recommend using with the app.
+                VStack {
+                    
+                    Image("HappyUser")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    
+                    HStack {
+                        Text("\"I'm so happy to hear the high-pitched bird songs again!\"")
+                            .font(.caption)
+                        Spacer()
+                    }
+                    
+                }
+                .padding()
+                
+                Text("""
+Both the app and its website [hearbirdsagain.org](https://hearbirdsagain.org) include documentation pertaining to the app. The in-app documentation includes detailed descriptions of the functions of app controls, but defers to the website for many details about other topics, such as [the problem the app solves](https://hearbirdsagain.org/problem-and-solutions/) and the [binaural headset](https://hearbirdsagain.org/binaural-headset/) that we strongly recommend using with the app. *If you want to make effective use of the app, please take the time to read both the in-app documentation and the web site.*
 
 You're currently viewing the app's *Info* tab, which includes several pages of documentation. You can navigate among the pages by swiping left and right or tapping to the left or right of the dots near the bottom of the tab.
 
