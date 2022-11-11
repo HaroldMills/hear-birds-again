1af3205fac0d25af24119398937b9f263d2151de
Harold Mills <harold.mills@gmail.com>
2022-11-11 10:43:20 -0500

    Add images to UI info page.

--- HearBirdsAgain/UiInfoPage.swift
+++ HearBirdsAgain/UiInfoPage.swift
@@ -18,9 +18,41 @@ struct UiInfoPage: View {
                 InfoPageTitle("The User Interface")
     
                 Text("""
-The ***Hear Birds Again*** user interface comprises three tabs, named *Home*, *Controls*, and *Info*. Only one tab is visible at a time, according to the icon you select at the bottom of the screen. You are currently viewing the *Info* tab.
+The ***Hear Birds Again*** user interface comprises three tabs, named *Home*, *Controls*, and *Info*. Only one tab is visible at a time, according to the icon selected in the *tab bar* at the bottom of the screen. The tab bar looks like this:
+""")
+                .padding()
+                
+                Image("TabBar")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding([.leading, .trailing])
 
-You'll probably use the *Home* tab most often, since it includes the most frequently-used app controls. The *Controls* tab includes less-frequently used controls. On both the *Home* and *Controls* tabs, most controls are accompanied by a small help button (a question mark in a circle) that you can tap to see a detailed explanation of the control's function.
+                Text("""
+You are currently viewing the *Info* tab.
+
+You'll probably use the *Home* tab most often, since it includes the most frequently-used app controls. The *Controls* tab includes less-frequently used controls.
+
+On both the *Home* and *Controls* tabs, most controls are accompanied by a small help button that looks like this:
+""")
+                .padding()
+                
+                HStack {
+                    
+                    Spacer()
+                    
+                    Image("HelpButton")
+                        .resizable()
+                        .frame(width: 32, height: 30
+                        )
+                        // .aspectRatio(contentMode: .fit)
+                        .padding([.leading, .trailing])
+                    
+                    Spacer()
+                    
+                }
+                
+                Text("""
+Tap a control's help button to see a detailed explanation of the control's function.
 """)
                 .padding()
                 
