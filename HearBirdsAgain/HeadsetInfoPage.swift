6c5bb216c315c478bfa1e76934b05e4e5390ed66
Harold Mills <harold.mills@gmail.com>
2022-12-13 16:11:24 -0500

    Revise binaural headset documentation.

--- HearBirdsAgain/HeadsetInfoPage.swift
+++ HearBirdsAgain/HeadsetInfoPage.swift
@@ -17,70 +17,49 @@ struct HeadsetInfoPage: View {
 
                 Group {
                     
-                    InfoPageTitle("The Binaural Headset")
+                    InfoPageTitle("Binaural Headsets")
                     
                     Text("""
-    For best results, ***Hear Birds Again*** should be used in conjunction with a special “binaural headset” that allows users to judge directions and distances of pitch-lowered bird songs in 3D space, and then actually “move toward and find the singers.”
-    """)
-                    .padding()
-                    
-                    InfoPageImage("HeadsetSide")
-                        .padding()
+For best results, Hear Birds Again should be used in conjunction with a special “binaural headset” that has microphones mounted near each ear and that allows users to judge directions and distances of pitch-lowered bird songs in 3D space, and then actually “move toward and find the singers.”
 
-                    Text("""
-Our recommended headset is [described here](https://hearbirdsagain.org/binaural-headset/) and ordering instructions are provided. It utilizes special low-noise microphones suitable for detecting subtle and distant bird songs. Be forewarned that some assembly will be required, including simple braiding of the cables and mounting the mics on the earphones.
+Unfortunately, there are no “perfect” binaural headsets currently available in the marketplace, but there are two workable solutions:
 """)
                     .padding()
                     
-                    InfoPageImage("BinauralHeadset")
+                    InfoPageSectionHeader("1. The Ambeo Smart Headset")
+                    
+                    InfoPageImage("AmbeoHeadset")
                         .padding()
                     
                     Text("""
-Unfortunately, no fully-assembled headsets of this type are currently available in the marketplace ([with one possible exception](https://hearbirdsagain.org/ambeo-smart-headset), although we are confident that high-quality plug-and-play headsets with built-in binaural mics eventually become available.
+Although no longer being manufactured, the Ambeo Smart Headset, [described here](https://hearbirdsagain.org/ambeo-smart-headset), is still available in the marketplace (at least for awhile) and provides an inexpensive “plug-and-play” binaural headset solution, with a purchase price as low as $50 US. The primary drawback is that it is equipped with microphones that are notably hissy, which can be annoying in quiet outdoor situations and may also be loud enough to obscure faint and distant bird songs. The Ambeo also plugs the ears somewhat, which may slightly muffle one’s normal hearing. In spite of these problems, the Ambeo provides a reasonably decent binaural listening experience.
 """)
                     .padding()
+                    
+                    InfoPageSectionHeader("2. The High-Fidelity Binaural Headset (kit)")
+                    
+                    InfoPageImage("HiFiHeadset")
+                        .padding()
+                    
+                    Text("""
+If you are a serious birder who desires a high-fidelity headset equipped with super low-noise microphones and earphones that do not plug one’s ears, we suggest you acquire our recommended **High-Fidelity Binaural Headset**, [described here](https://hearbirdsagain.org/high-fidelity-binaural-headset), which is available in kit form. While it is expensive in comparison to the Ambeo (around $175 vs $50) and requires some assembly (mostly the final twisting of cables), it undoubtedly provides the most pleasurable user-experience in the field, including superior “localization” of the high-pitched singers. If you’re really into birds and are unafraid of the assembly aspect, we strongly suggest you take this route.
+""")
+                    .padding()
+                    
                 }
-                    
-                Text("WARNING!: It is important to properly adjust settings for the RODE AI-Micro audio interface in order to prevent loud feedback squeal.")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                    .foregroundColor(.red)
-                    .padding([.leading, .top, .trailing])
-                    
-                Text("""
-Our recommended headset requires the use of the RODE AI-Micro interface, which will route the headset mics inputs into one’s mobile device and the device’s output (the pitch-lowered bird songs) back to the Koss Earphones. In order for this to work properly, it will be necessary for you to download RODE’s RODE Central application and then adjust the settings. The procedure is as follows:
-
-1. Download RODE Central and then open the app.
-                
-2. Connect your RODE AI-Micro interface to your mobile device. RODE Central should then automatically detect the audio interface.
-                
-3. TURN OFF the “Direct Monitor” option (the bottommost setting). CAUTION: If left enabled, you will probably experience loud feedback squeal!
-                
-4. Adjust other settings as shown in the following screenshot of the RODE Central user interface:
-""")
-                .padding()
-                    
-                InfoPageImage("RodeCentralConfig")
-                    .hbaScreenshot()
-                    .padding()
-                    
-                Text("""
-Once the direct monitoring setting has been disabled, everything should work fine and you should not have to use the RODE Central app again, except perhaps to occasionally check for updates to the RODE AI-Micro firmware (RODE Central automatically detects if your current firmware is out of date).
-""")
-                .padding()
                     
                 InfoPageSectionHeader("Other Input and Output Options")
                 
                 Text("""
-While our binaural headset is vastly superior to other options, it is possible to test our app using your mobile device’s built-in mics along with wired or wireless earbuds and headphones. Be aware, however, that this is not an optimal solution and results will vary greatly depending on a number of factors.
-
-While our binaural headset is vastly superior to other options, it is possible to test our app using your mobile device’s built-in mics along with wired or wireless earbuds and headphones. Be aware, however, that this is not an optimal solution and results will vary greatly depending on a number of factors.
+    While true binaural headset options such as those described above are necessary for a satisfying “3D” experience in the field, it is possible to test our app using your mobile device’s built-in mics along with wired or wireless earbuds and headphones. Be aware, however, that this is not an optimal solution and results will vary greatly depending on a number of factors.
 
 The biggest problem is that the built-in mics do not provide accurate spatial audio, so it will be nearly impossible to determine the directions and distances of incoming pitch-lowered songs. The built-in mics also have high internal noise, which can mask faint bird songs.
 
 Furthermore, if bluetooth earbuds or headphones are used that have built-in mics, those mics are likely to be enabled and will probably result in low quality audio, with no spatial sense whatsoever.
 
-To make matters worse, bluetooth has "latency issues," meaning there is usually a noticeable delay in the transmission of pitch-lowered signals. This may be particularly annoying if you are with other people and the high-pitched portions of their voices are delayed with respect to the lower pitched aspects of their voices that you are hearing normally. For testing without the recommended binaural headset, we advise using wired earbuds (without mics), coupled with the mobile device's built-in mics. Note that if you're using an iphone, you should orient it horizontally so as to engage the internal stereo mics. This will result in a pleasing sense of space though without the accuracy necessary to actually find and observe singing birds.
+To make matters worse, bluetooth has "latency issues," meaning there is usually a noticeable delay in the transmission of pitch-lowered signals. This may be particularly annoying if you are with other people and the high-pitched portions of their voices are delayed with respect to the lower pitched aspects of their voices that you are hearing normally.
+
+For testing without a binaural headset, we advise using wired earbuds (without mics), coupled with the mobile device's built-in mics. Note that if you're using an iPhone, you should orient it horizontally so as to engage the internal stereo mics. This will result in a pleasing sense of space though without the accuracy necessary to actually find and observe singing birds.
 """)
                 .padding()
                     
--- HearBirdsAgain/OutputLevelHelp.swift
+++ HearBirdsAgain/OutputLevelHelp.swift
@@ -22,7 +22,7 @@ struct OutputLevelHelp: View {
             Text("""
 The *Output Level* meter shows the power level of the audio that ***Hear Birds Again*** is currently sending to the iOS operating system. The level reflects all of the processing that the app performs, including filtering, pitch shifting, amplification, and balance processing. It *does not* reflect amplification that iOS performs according to the *iOS Audio Volume* control.\n\nThe meter has one bar when output is monaural, and two bars when output is stereo.
 
-You can look at the meter to confirm that ***Hear Birds Again*** is receiving audio from the mics, either those mounted on the recommended [binaural headset](https://hearbirdsagain.org/binaural-headset/) or those built into your iOS device or auxiliary earbuds or headphones. If the app is receiving audio, you will see activity in the meter. If the app is not receiving audio, the meter will be still.
+You can look at the meter to confirm that ***Hear Birds Again*** is receiving audio from the mics, either those mounted on a [binaural headset](https://hearbirdsagain.org/binaural-headset/) or those built into your iOS device or auxiliary earbuds or headphones. If the app is receiving audio, you will see activity in the meter. If the app is not receiving audio, the meter will be still.
 
 If the output level displayed in the meter is consistently low (towards the left end of the green region, say even when you make pishing sounds) or high (jumping into the red region), try raising or lowering the gain on the *More Controls* tab to suit.
 """)
